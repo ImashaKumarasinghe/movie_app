f254bc103ecae1bcb0fa3265d603679a42c1e05c
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2026-03-21 00:52:24 +0530

    refactor: rename MyApp to MovieApp and update main function and apply dark mode to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MovieApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class MovieApp extends StatelessWidget {
+  const MovieApp();
   
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Movie App',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.white,
+        scaffoldBackgroundColor: Colors.grey[900],
+        textTheme: const TextTheme(
+          headlineSmall: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+          bodyLarge: TextStyle(color: Colors.white70),
+          bodyMedium: TextStyle(color: Colors.white70),
+          bodySmall: TextStyle(color: Colors.white70)
+        ),
+        appBarTheme: const AppBarTheme(
+          iconTheme: IconThemeData(color: Colors.white),
+        )),
+      home: const Center(
+        child: Text('Movie App')),
+    );
   }
+}
 
