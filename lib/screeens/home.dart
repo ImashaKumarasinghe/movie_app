10d48a408311857ac08becf31e458465600f54e6
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2026-03-21 11:08:56 +0530

    feat: enhance HomeScreen with bottom navigation and page view functionality

--- lib/screeens/home.dart
+++ lib/screeens/home.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+
 class HomeScreen extends StatefulWidget {
     const HomeScreen({super.key});
 
     @override
-    State<HomeScreen> createState() => HomeScreenState();
-
+    State<HomeScreen> createState() => _HomeScreenState();
 }
-class HomeScreenState extends State<HomeScreen> {
-    final PageController pageController = PageController(initialPage: 0);
-    late int currentIndex = 0;
 
+class _HomeScreenState extends State<HomeScreen> {
+    final PageController _pageController = PageController(initialPage: 0);
+    int _selectedIndex = 0;
+
+    @override
+    void dispose() {
+        _pageController.dispose();
+        super.dispose();
+    }
 
     @override
     Widget build(BuildContext context) {
@@ -18,12 +24,40 @@ class HomeScreenState extends State<HomeScreen> {
                 title: const Text('Movie App'),
             ),
             body: PageView(
-                 children :const [
-                     Center(
+                controller: _pageController,
+                onPageChanged: (index) {
+                    setState(() {
+                        _selectedIndex = index;
+                    });
+                },
+                children: const [
+                    Center(
                         child: Text('Welcome to Movie App 1!'),
-                     ),
-                     Center(
+                    ),
+                    Center(
                         child: Text('Welcome to Movie App 2!'),
+                    ),
+                ],
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+                type: BottomNavigationBarType.fixed,
+                currentIndex: _selectedIndex,
+                selectedItemColor: Colors.white,
+                unselectedItemColor: Colors.blueGrey,
+                onTap: (index) {
+                    setState(() {
+                        _selectedIndex = index;
+                    });
+                    _pageController.jumpToPage(index);
+                },
+                items: const [
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.home),
+                        label: 'Home',
+                    ),
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.movie),
+                        label: 'Movies',
                     ),
                 ],
             ),
